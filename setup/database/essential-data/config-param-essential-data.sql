2d4e50c9a5c3cc6b68cb3fe15f5226956c8e4f54
Jason Kong <jason.yh.kong@pccw.com>
2019-07-29 12:20:41 +0800

    remove skyexchange sql

--- setup/database/essential-data/config-param-essential-data.sql
+++ setup/database/essential-data/config-param-essential-data.sql
@@ -1,28 +1,20 @@
-# which group can grant which group
-insert into CONFIG_PARAM(CONFIG_GROUP, CONFIG_KEY, CONFIG_VALUE, CONFIG_VALUE_TYPE, CREATEDATE, CREATEBY, MODIFYDATE, MODIFYBY)
-values ('USER_GROUP_CREATE_MAPPING', 'ROOT', 'ADMIN,USER,C_ADMIN,C_USER', 'String', current_timestamp, 0, current_timestamp, 0);
-insert into CONFIG_PARAM(CONFIG_GROUP, CONFIG_KEY, CONFIG_VALUE, CONFIG_VALUE_TYPE, CREATEDATE, CREATEBY, MODIFYDATE, MODIFYBY)
-values ('USER_GROUP_CREATE_MAPPING', 'ADMIN', 'ADMIN,USER,C_ADMIN,C_USER', 'String', current_timestamp, 0, current_timestamp, 0);
-insert into CONFIG_PARAM(CONFIG_GROUP, CONFIG_KEY, CONFIG_VALUE, CONFIG_VALUE_TYPE, CREATEDATE, CREATEBY, MODIFYDATE, MODIFYBY)
-values ('USER_GROUP_CREATE_MAPPING', 'C_ADMIN', 'C_ADMIN,C_USER', 'String', current_timestamp, 0, current_timestamp, 0);
-
-# Site Config
+-- Site Config
 insert into CONFIG_PARAM(CONFIG_GROUP, CONFIG_KEY, CONFIG_VALUE, CONFIG_VALUE_TYPE, CREATEDATE, CREATEBY, MODIFYDATE, MODIFYBY)
 values ('SITE', 'passwordResetOtpLifespanInMin', '30', 'Integer', current_timestamp, 0, current_timestamp, 0);
 
 insert into CONFIG_PARAM(CONFIG_GROUP, CONFIG_KEY, CONFIG_VALUE, CONFIG_VALUE_TYPE, CREATEDATE, CREATEBY, MODIFYDATE, MODIFYBY)
-values ('SITE', 'appName', 'SkyExchange', 'String', current_timestamp, 0, current_timestamp, 0);
+values ('SITE', 'appName', 'ServiceDesk', 'String', current_timestamp, 0, current_timestamp, 0);
 
 insert into CONFIG_PARAM(CONFIG_GROUP, CONFIG_KEY, CONFIG_VALUE, CONFIG_VALUE_TYPE, CREATEDATE, CREATEBY, MODIFYDATE, MODIFYBY)
-values ('SITE', 'givenDomain', 'noc01.oss.pccw.com', 'String', current_timestamp, 0, current_timestamp, 0);
+values ('SITE', 'givenDomain', '', 'String', current_timestamp, 0, current_timestamp, 0);
 insert into CONFIG_PARAM(CONFIG_GROUP, CONFIG_KEY, CONFIG_VALUE, CONFIG_VALUE_TYPE, CREATEDATE, CREATEBY, MODIFYDATE, MODIFYBY)
-values ('SITE', 'cronjobHostname', 'yecdevvm-scpuat01', 'String', current_timestamp, 0, current_timestamp, 0);
+values ('SITE', 'cronjobHostname', 'yecvm_saapp01', 'String', current_timestamp, 0, current_timestamp, 0);
 insert into CONFIG_PARAM(CONFIG_GROUP, CONFIG_KEY, CONFIG_VALUE, CONFIG_VALUE_TYPE, CREATEDATE, CREATEBY, MODIFYDATE, MODIFYBY)
-values ('SITE', 'prodHostname', 'yecvm-scpapp01', 'String', current_timestamp, 0, current_timestamp, 0);
+values ('SITE', 'prodHostname', 'yecvm_saapp01', 'String', current_timestamp, 0, current_timestamp, 0);
 insert into CONFIG_PARAM(CONFIG_GROUP, CONFIG_KEY, CONFIG_VALUE, CONFIG_VALUE_TYPE, CREATEDATE, CREATEBY, MODIFYDATE, MODIFYBY)
-values ('SITE', 'prodStandbyHostname', 'jecvm-scpapp01', 'String', current_timestamp, 0, current_timestamp, 0);
+values ('SITE', 'prodStandbyHostname', 'jecvm_saapp01', 'String', current_timestamp, 0, current_timestamp, 0);
 insert into CONFIG_PARAM(CONFIG_GROUP, CONFIG_KEY, CONFIG_VALUE, CONFIG_VALUE_TYPE, CREATEDATE, CREATEBY, MODIFYDATE, MODIFYBY)
-values ('SITE', 'uatHostname', 'yecdevvm-scpuat01', 'String', current_timestamp, 0, current_timestamp, 0);
+values ('SITE', 'uatHostname', 'yecdevvm-sauat01', 'String', current_timestamp, 0, current_timestamp, 0);
 
 insert into CONFIG_PARAM(CONFIG_GROUP, CONFIG_KEY, CONFIG_VALUE, CONFIG_VALUE_TYPE, CREATEDATE, CREATEBY, MODIFYDATE, MODIFYBY)
 values ('SITE', 'mailHost', 'smtp.pccw.com', 'String', current_timestamp, 0, current_timestamp, 0);
@@ -31,26 +23,6 @@ values ('SITE', 'mailPort', '25', 'Integer', current_timestamp, 0, current_times
 insert into CONFIG_PARAM(CONFIG_GROUP, CONFIG_KEY, CONFIG_VALUE, CONFIG_VALUE_TYPE, CREATEDATE, CREATEBY, MODIFYDATE, MODIFYBY)
 values ('SITE', 'mailUsername', 'sdadmin@pccw.com', 'String', current_timestamp, 0, current_timestamp, 0);
 
-# NFM Team Contact
-insert into CONFIG_PARAM(CONFIG_GROUP, CONFIG_KEY, CONFIG_VALUE, CONFIG_VALUE_TYPE, CREATEDATE, CREATEBY, MODIFYDATE, MODIFYBY)
-values ('NFM', 'email', 'ka-chun.cheung@pccw.com', 'String', current_timestamp, 0, current_timestamp, 0);
-
-# visit location notification email
-insert into CONFIG_PARAM(CONFIG_GROUP, CONFIG_KEY, CONFIG_VALUE, CONFIG_VALUE_TYPE, CREATEDATE, CREATEBY, MODIFYDATE, MODIFYBY)
-values ('VISIT_LOC_EMAIL', 'VTA', 'pccwnfmvta@pccw.com', 'String', current_timestamp, 0, current_timestamp, 0);
-insert into CONFIG_PARAM(CONFIG_GROUP, CONFIG_KEY, CONFIG_VALUE, CONFIG_VALUE_TYPE, CREATEDATE, CREATEBY, MODIFYDATE, MODIFYBY)
-values ('VISIT_LOC_EMAIL', 'JBY', 'pccwnfmjby@pccw.com', 'String', current_timestamp, 0, current_timestamp, 0);
-insert into CONFIG_PARAM(CONFIG_GROUP, CONFIG_KEY, CONFIG_VALUE, CONFIG_VALUE_TYPE, CREATEDATE, CREATEBY, MODIFYDATE, MODIFYBY)
-values ('VISIT_LOC_EMAIL', 'LKT2F', 'pccwnfmlkt@pccw.com', 'String', current_timestamp, 0, current_timestamp, 0);
-insert into CONFIG_PARAM(CONFIG_GROUP, CONFIG_KEY, CONFIG_VALUE, CONFIG_VALUE_TYPE, CREATEDATE, CREATEBY, MODIFYDATE, MODIFYBY)
-values ('VISIT_LOC_EMAIL', 'LKT5F', 'pccwnfmlkt@pccw.com', 'String', current_timestamp, 0, current_timestamp, 0);
-insert into CONFIG_PARAM(CONFIG_GROUP, CONFIG_KEY, CONFIG_VALUE, CONFIG_VALUE_TYPE, CREATEDATE, CREATEBY, MODIFYDATE, MODIFYBY)
-values ('VISIT_LOC_EMAIL', 'IAC', 'pccwnfmiac@pccw.com', 'String', current_timestamp, 0, current_timestamp, 0);
-insert into CONFIG_PARAM(CONFIG_GROUP, CONFIG_KEY, CONFIG_VALUE, CONFIG_VALUE_TYPE, CREATEDATE, CREATEBY, MODIFYDATE, MODIFYBY)
-values ('VISIT_LOC_EMAIL', 'MCX3F', 'pccwnfmmcx@pccw.com', 'String', current_timestamp, 0, current_timestamp, 0);
-insert into CONFIG_PARAM(CONFIG_GROUP, CONFIG_KEY, CONFIG_VALUE, CONFIG_VALUE_TYPE, CREATEDATE, CREATEBY, MODIFYDATE, MODIFYBY)
-values ('VISIT_LOC_EMAIL', 'MCX6F', 'pccwnfmmcx@pccw.com', 'String', current_timestamp, 0, current_timestamp, 0);
-
-# Cronjob Team
+-- Cronjob Team
 insert into CONFIG_PARAM(CONFIG_GROUP, CONFIG_KEY, CONFIG_VALUE, CONFIG_VALUE_TYPE, CREATEDATE, CREATEBY, MODIFYDATE, MODIFYBY)
 values ('CRONJOB', 'errorEmail', 'jason.yh.kong@pccw.com', 'String', current_timestamp, 0, current_timestamp, 0);
--- setup/database/essential-data/cron-job-essential-data.sql
+++
@@ -1,8 +0,0 @@
-insert into CRON_JOB(JOB_GROUP, JOB_NAME, JOB_CLASS, CRON_EXP, STATUS, MANDATORY, CREATEBY, MODIFYBY)
-values ('AccessRequest', 'NocAutoCpAccessRequestJob', 'com.hkt.btu.noc.core.job.NocAutoCpAccessRequestJob', 
-        '0 0 1 ? * * *', 'A', 'N', 0, 0);
-
-insert into CRON_JOB(JOB_GROUP, JOB_NAME, JOB_CLASS, CRON_EXP, STATUS, MANDATORY, CREATEBY, MODIFYBY)
-values ('AccessRequest', 'NocGenerateAccessRequestHashedIdJob', 'com.hkt.btu.noc.core.job.NocGenerateAccessRequestHashedIdJob', 
-        '0 15 0/4 ? * * *', 'A', 'Y', 0, 0);
-
