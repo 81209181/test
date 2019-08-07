986e566465383129ecdd9ef6881d7d20ab6328d1
80883564 <Dennis.WF.Chen@pccw.com>
2019-08-07 15:20:42 +0800

    build BtuSchedulerService

---
+++ btucore/src/main/java/com/hkt/btu/common/core/service/impl/BtuCronJobProfileServiceImpl.java
@@ -0,0 +1,11 @@
+package com.hkt.btu.common.core.service.impl;
+
+import com.hkt.btu.common.core.service.BtuCronJobProfileService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class BtuCronJobProfileServiceImpl implements BtuCronJobProfileService {
+    private static final Logger LOG = LogManager.getLogger(BtuCronJobProfileServiceImpl.class);
+
+
+}
--- sdcore/src/main/java/com/hkt/btu/sd/core/service/impl/SdSchedulerServiceImpl.java
+++ sdcore/src/main/java/com/hkt/btu/sd/core/service/impl/SdSchedulerServiceImpl.java
@@ -1,34 +1,30 @@
 package com.hkt.btu.sd.core.service.impl;
 
-import com.hkt.btu.sd.core.exception.InvalidInputException;
-import com.hkt.btu.sd.core.service.SdCronJobLogService;
+import com.hkt.btu.common.core.exception.InvalidInputException;
+import com.hkt.btu.common.core.service.impl.BtuSchedulerServiceImpl;
 import com.hkt.btu.sd.core.service.SdCronJobProfileService;
 import com.hkt.btu.sd.core.service.SdSchedulerService;
 import com.hkt.btu.sd.core.service.bean.SdCronJobInstBean;
 import com.hkt.btu.sd.core.service.bean.SdCronJobProfileBean;
 import com.hkt.btu.sd.core.service.populator.SdCronJobInstBeanPopulator;
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.quartz.*;
 import org.quartz.impl.matchers.GroupMatcher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
-import org.springframework.scheduling.quartz.JobDetailFactoryBean;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
 import javax.annotation.Resource;
-import java.text.ParseException;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
-public class SdSchedulerServiceImpl implements SdSchedulerService {
+public class SdSchedulerServiceImpl extends BtuSchedulerServiceImpl implements SdSchedulerService {
     private static final Logger LOG = LogManager.getLogger(SdSchedulerServiceImpl.class);
 
 
@@ -39,8 +35,6 @@ public class SdSchedulerServiceImpl implements SdSchedulerService {
 
     @Resource(name = "cronJobProfileService")
     SdCronJobProfileService sdCronJobProfileService;
-    @Resource(name = "cronJobLogService")
-    SdCronJobLogService sdCronJobLogService;
 
     @Resource(name = "cronJobInstBeanPopulator")
     SdCronJobInstBeanPopulator sdCronJobInstBeanPopulator;
@@ -98,7 +92,6 @@ public class SdSchedulerServiceImpl implements SdSchedulerService {
                 jobBeanList.add(jobBean);
             }
         }
-
         return jobBeanList;
     }
 
@@ -136,79 +129,10 @@ public class SdSchedulerServiceImpl implements SdSchedulerService {
 
         return jobBean;
     }
-
-    private JobKey getJobKey(String keyGroup, String keyName) throws SchedulerException, InvalidInputException {
-        Scheduler scheduler = schedulerFactoryBean.getScheduler();
-
-        for (String groupName : scheduler.getJobGroupNames()) {
-            for (JobKey jobKey : scheduler.getJobKeys(GroupMatcher.jobGroupEquals(groupName))) {
-                if( StringUtils.equals(jobKey.getGroup(), keyGroup)
-                        && StringUtils.equals(jobKey.getName(), keyName) ){
-                    return jobKey;
-                }
-            }
-        }
-
-        throw new InvalidInputException("Invalid job key (" + keyGroup + ", " + keyName + ").");
-    }
-
-    private JobDetail getJobDetail(JobKey jobKey) throws SchedulerException {
-        Scheduler scheduler = schedulerFactoryBean.getScheduler();
-        JobDetail jobDetail = scheduler.getJobDetail(jobKey);
-        if(jobDetail==null){
-            throw new SchedulerException("Job detail not found (" + jobKey + ").");
-        }
-        return jobDetail;
-    }
-
-    @Override
-    public void resumeJob(String keyGroup, String keyName) throws SchedulerException, InvalidInputException {
-        JobKey targetJobKey = getJobKey(keyGroup, keyName);
-        JobDetail jobDetail = getJobDetail(targetJobKey);
-
-        Scheduler scheduler = schedulerFactoryBean.getScheduler();
-        scheduler.resumeJob(targetJobKey);
-
-        LOG.info("Resumed job: " + targetJobKey);
-        sdCronJobLogService.logUserResumeJob(jobDetail);
-    }
-
-    @Override
-    public void pauseJob(String keyGroup, String keyName) throws SchedulerException, InvalidInputException {
-        JobKey targetJobKey = getJobKey(keyGroup, keyName);
-        JobDetail jobDetail = getJobDetail(targetJobKey);
-
-        Scheduler scheduler = schedulerFactoryBean.getScheduler();
-        scheduler.pauseJob(targetJobKey);
-
-        LOG.info("Paused job: " + targetJobKey);
-        sdCronJobLogService.logUserPauseJob(jobDetail);
-    }
-
-    @Override
-    public void triggerJob(String keyGroup, String keyName) throws SchedulerException, InvalidInputException {
-        JobKey targetJobKey = getJobKey(keyGroup, keyName);
-        JobDetail jobDetail = getJobDetail(targetJobKey);
-
-        Scheduler scheduler = schedulerFactoryBean.getScheduler();
-        scheduler.triggerJob(targetJobKey);
-
-        LOG.info("Triggered job: " + targetJobKey);
-        sdCronJobLogService.logUserTriggerJob(jobDetail);
-    }
-
     @Override
     public void scheduleJob(String keyGroup, String keyName) throws SchedulerException, InvalidInputException, ClassNotFoundException {
         SdCronJobProfileBean jobProfileBean = sdCronJobProfileService.getProfileBeanByGrpAndName(keyGroup, keyName);
         scheduleJob(jobProfileBean);
-    }
-
-    @Override
-    public void destroyJob(String keyGroup, String keyName) throws SchedulerException, InvalidInputException {
-        JobKey targetJobKey = getJobKey(keyGroup, keyName);
-        Scheduler scheduler = schedulerFactoryBean.getScheduler();
-        scheduler.deleteJob(targetJobKey);
-        LOG.info("Destroyed job: " + targetJobKey);
     }
 
     private void scheduleJob(SdCronJobProfileBean jobProfileBean) throws SchedulerException, InvalidInputException, ClassNotFoundException {
@@ -248,54 +172,6 @@ public class SdSchedulerServiceImpl implements SdSchedulerService {
         }
     }
 
-    /**
-     * Create Quartz Job.
-     *
-     * @param jobClass  Class whose executeInternal() method needs to be called.
-     * @param context   Spring application context.
-     * @param jobName   Job name.
-     * @param jobGroup  Job group.
-     * @return JobDetail object
-     */
-    private JobDetail createJob(Class<? extends QuartzJobBean> jobClass,
-                                ApplicationContext context, String jobName, String jobGroup) {
-        JobDetailFactoryBean factoryBean = new JobDetailFactoryBean();
-        factoryBean.setJobClass(jobClass);
-        factoryBean.setDurability(false);
-        factoryBean.setApplicationContext(context);
-        factoryBean.setName(jobName);
-        factoryBean.setGroup(jobGroup);
-
-        // set job data map
-        JobDataMap jobDataMap = new JobDataMap();
-        jobDataMap.put(jobName + jobGroup, jobClass.getName());
-        factoryBean.setJobDataMap(jobDataMap);
-        factoryBean.afterPropertiesSet();
-
-        return factoryBean.getObject();
-    }
-
-    /**
-     * Create cron trigger.
-     *
-     * @param triggerName        Trigger name.
-     * @param startTime          Trigger start time.
-     * @param cronExpression     Cron expression.
-     * @return {@link CronTrigger}
-     */
-    private CronTrigger createCronTrigger(String triggerName, Date startTime, String cronExpression) {
-        CronTriggerFactoryBean factoryBean = new CronTriggerFactoryBean();
-        factoryBean.setName(triggerName);
-        factoryBean.setStartTime(startTime);
-        factoryBean.setCronExpression(cronExpression);
-        factoryBean.setMisfireInstruction(SimpleTrigger.MISFIRE_INSTRUCTION_FIRE_NOW);
-        try {
-            factoryBean.afterPropertiesSet();
-        } catch (ParseException e) {
-            LOG.error(e.getMessage(), e);
-        }
-        return factoryBean.getObject();
-    }
 
 
 
