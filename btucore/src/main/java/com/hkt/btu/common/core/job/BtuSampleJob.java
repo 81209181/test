3975e83843212336e0f15404a3c26deca7fe16a2
80883564 <Dennis.WF.Chen@pccw.com>
2019-08-16 12:16:25 +0800

    aop all job .

--- btucore/src/main/java/com/hkt/btu/common/core/job/BtuSampleJob.java
+++ btucore/src/main/java/com/hkt/btu/common/core/job/BtuSampleJob.java
@@ -2,12 +2,14 @@ package com.hkt.btu.common.core.job;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 import java.time.LocalDateTime;
 
+@DisallowConcurrentExecution
 public class BtuSampleJob extends QuartzJobBean {
     private static final Logger LOG = LogManager.getLogger(BtuSampleJob.class);
 
