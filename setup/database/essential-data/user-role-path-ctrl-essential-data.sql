8eb6d8b30d86549146ce798c324ed8541847b8e0
JasonTan <jason.sp.tan@pccw.com>
2019-08-20 19:25:35 +0800

    [SERVICEDESK-58] Create path control (SQL)

---
+++ setup/database/essential-data/user-role-path-ctrl-essential-data.sql
@@ -0,0 +1,6 @@
+-- User Role Path Ctrl
+insert into USER_ROLE_PATH_CTRL (ROLE_ID, PATH_CTRL_ID, CREATEBY, REMARKS)
+values('1', 1, '81149189', 'Basic');
+
+insert into USER_ROLE_PATH_CTRL (ROLE_ID, PATH_CTRL_ID, CREATEBY, REMARKS)
+values('2', 2, '81149189', 'System Admin');
---
+++ setup/database/sample-data/user-role-path-ctrl-sample-data.sql
@@ -0,0 +1,14 @@
+-- User Role Path Ctrl
+insert into USER_ROLE_PATH_CTRL (ROLE_ID, PATH_CTRL_ID, CREATEBY, REMARKS)
+values('1', 1, '81149189', 'Basic');
+
+insert into USER_ROLE_PATH_CTRL (ROLE_ID, PATH_CTRL_ID, CREATEBY, REMARKS)
+values('2', 2, '81149189', 'System Admin');
+
+insert into USER_ROLE_PATH_CTRL (ROLE_ID, PATH_CTRL_ID, CREATEBY, REMARKS)
+values('3', 3, '81149189', 'Fault Page');
+
+insert into USER_ROLE_PATH_CTRL (ROLE_ID, PATH_CTRL_ID, CREATEBY, REMARKS)
+values('6', 4, '81149189', 'Manager User');
+
+
