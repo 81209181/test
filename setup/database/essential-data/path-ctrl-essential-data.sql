28f4e63d781690cf239462332cf0e485d6fecad0
JasonTan <jason.sp.tan@pccw.com>
2019-08-23 19:11:21 +0800

    [SERVICEDESK-58] Create path Control(SQL)

--- setup/database/essential-data/path-ctrl-essential-data.sql
+++ setup/database/essential-data/path-ctrl-essential-data.sql
@@ -1,5 +1,5 @@
 -- AntPath mapping control
-insert into PATH_CTRL(PATH, STATUS, DESCRIPTION,CREATEBY, MODIFYBY) values('/user/my-account', 'A', 'Basic Path', '81149189', '81149189');
+insert into PATH_CTRL(PATH, STATUS, DESCRIPTION,CREATEBY, MODIFYBY) values('/user/**', 'A', 'Basic Path', '81149189', '81149189');
 insert into PATH_CTRL(PATH, STATUS, DESCRIPTION,CREATEBY, MODIFYBY) values('/**', 'A', 'System Admin', '81149189', '81149189');
 insert into PATH_CTRL(PATH, STATUS, DESCRIPTION,CREATEBY, MODIFYBY) values('/fault/**', 'A', 'CS Agent', '81149189', '81149189');
-insert into PATH_CTRL(PATH, STATUS, DESCRIPTION,CREATEBY, MODIFYBY) values('/admin/manage-user', 'A', 'Team Head', '81149189', '81149189');
+insert into PATH_CTRL(PATH, STATUS, DESCRIPTION,CREATEBY, MODIFYBY) values('/admin/manage-user/**', 'A', 'Team Head', '81149189', '81149189');
