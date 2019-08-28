4509fe9f4540c51814bace70b1b1338208ea209d
Jason Kong <jason.yh.kong@pccw.com>
2019-08-28 12:26:13 +0800

    [SERVICEDESK-56] Add user role(s) - remove comment out code

--- sdcore/src/main/java/com/hkt/btu/sd/core/service/populator/SdUserBeanPopulator.java
+++ sdcore/src/main/java/com/hkt/btu/sd/core/service/populator/SdUserBeanPopulator.java
@@ -3,16 +3,12 @@ package com.hkt.btu.sd.core.service.populator;
 
 import com.hkt.btu.common.core.service.populator.AbstractBeanPopulator;
 import com.hkt.btu.sd.core.dao.entity.SdUserEntity;
-import com.hkt.btu.sd.core.dao.entity.SdUserGroupEntity;
 import com.hkt.btu.sd.core.dao.entity.SdUserRoleEntity;
 import com.hkt.btu.sd.core.service.bean.SdUserBean;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.util.CollectionUtils;
 
-import javax.annotation.Resource;
-import java.io.UnsupportedEncodingException;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -34,17 +30,9 @@ public class SdUserBeanPopulator extends AbstractBeanPopulator<SdUserBean> {
         target.setName(source.getName());
         target.setEmail(source.getEmail());
         target.setCompanyId(source.getCompanyId());
-
-        // TODO: Encryption function will be done later
-        // encrypted data
         target.setLdapDomain(source.getLdapDomain());
         target.setMobile(source.getMobile());
         target.setStaffId(source.getStaffId());
-        /*String decryptedMobile = sdSensitiveDataService.decryptToStringSafe(source.getMobile());
-        target.setMobile(decryptedMobile);
-        // TODO: Encryption function will be done later
-        String decryptedStaffId = sdSensitiveDataService.decryptToStringSafe(source.getStaffId());
-        target.setStaffId(decryptedStaffId);*/
     }
 
     public void populate(List<SdUserRoleEntity> userRoleEntityList, SdUserBean target) {
