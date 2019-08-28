ac36f0bc053f9adc3a8ed7b472acfb633a2527af
Jason Kong <jason.yh.kong@pccw.com>
2019-08-28 12:16:27 +0800

    [SERVICEDESK-57] Create Roles(SQL) - add parent for sys_admin

--- setup/database/essential-data/user-role-essential-data.sql
+++ setup/database/essential-data/user-role-essential-data.sql
@@ -3,7 +3,7 @@ insert into user_role (ROLE_ID, ROLE_DESC, PARENT_ROLE_ID, STATUS, CREATEBY, MOD
 values ('BASIC', 'Basic', NULL, 'A', '81149189', '81149189','BASIC ROLE');
 
 insert into user_role (ROLE_ID, ROLE_DESC, PARENT_ROLE_ID, STATUS, CREATEBY, MODIFYBY, REMARKS)
-values ('SYS_ADMIN', 'System Admin', NULL, 'A', '81149189', '81149189', 'System Admin');
+values ('SYS_ADMIN', 'System Admin', 'BASIC', 'A', '81149189', '81149189', 'System Admin');
 
 insert into user_role (ROLE_ID, ROLE_DESC, PARENT_ROLE_ID, STATUS, CREATEBY, MODIFYBY, REMARKS)
 values ('TEAM_HEAD', 'Team Head', 'BASIC', 'A', '81149189', '81149189', 'Team Head');
