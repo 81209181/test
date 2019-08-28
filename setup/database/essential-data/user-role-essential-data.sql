6207a5e57a7af5185f01165bd778918b6b040473
Jason Kong <jason.yh.kong@pccw.com>
2019-08-28 11:21:12 +0800

    [SERVICEDESK-57] Create Roles(SQL) - re-organize sample/essential data

--- setup/database/essential-data/user-role-essential-data.sql
+++ setup/database/essential-data/user-role-essential-data.sql
@@ -4,3 +4,9 @@ values ('BASIC', 'Basic', NULL, 'A', '81149189', '81149189','BASIC ROLE');
 
 insert into user_role (ROLE_ID, ROLE_DESC, PARENT_ROLE_ID, STATUS, CREATEBY, MODIFYBY, REMARKS)
 values ('SYS_ADMIN', 'System Admin', NULL, 'A', '81149189', '81149189', 'System Admin');
+
+insert into user_role (ROLE_ID, ROLE_DESC, PARENT_ROLE_ID, STATUS, CREATEBY, MODIFYBY, REMARKS)
+values ('TEAM_HEAD', 'Team Head', 'BASIC', 'A', '81149189', '81149189', 'Team Head');
+
+insert into user_role (ROLE_ID, ROLE_DESC, PARENT_ROLE_ID, STATUS, CREATEBY, MODIFYBY, REMARKS)
+values ('CS_AGENT', 'Agent', 'BASIC', 'A', '81149189', '81149189', 'AGENT');
--- setup/database/sample-data/user-role-sample-data.sql
+++ setup/database/sample-data/user-role-sample-data.sql
@@ -1,21 +1,9 @@
 -- User Role
 insert into user_role (ROLE_ID, ROLE_DESC, PARENT_ROLE_ID, STATUS, CREATEBY, MODIFYBY, REMARKS)
-values ('BASIC', 'Basic', NULL, 'A', '81149189', '81149189', 'BASIC ROLE');
-
-insert into user_role (ROLE_ID, ROLE_DESC, PARENT_ROLE_ID, STATUS, CREATEBY, MODIFYBY, REMARKS)
-values ('SYS_ADMIN', 'System Admin', NULL, 'A', '81149189', '81149189', 'System Admin');
-
-insert into user_role (ROLE_ID, ROLE_DESC, PARENT_ROLE_ID, STATUS, CREATEBY, MODIFYBY, REMARKS)
-values ('CS_AGENT', 'Agent', 'BASIC', 'A', '81149189', '81149189', 'AGENT');
-
-insert into user_role (ROLE_ID, ROLE_DESC, PARENT_ROLE_ID, STATUS, CREATEBY, MODIFYBY, REMARKS)
 values ('TEAM_A', 'Team A', 'CS_AGENT', 'A', '81149189', '81149189', 'Team A');
 
 insert into user_role (ROLE_ID, ROLE_DESC, PARENT_ROLE_ID, STATUS, CREATEBY, MODIFYBY, REMARKS)
 values ('TEAM_B', 'Team B', 'CS_AGENT', 'A', '81149189', '81149189', 'Team B');
-
-insert into user_role (ROLE_ID, ROLE_DESC, PARENT_ROLE_ID, STATUS, CREATEBY, MODIFYBY, REMARKS)
-values ('TEAM_HEAD', 'Team Head', 'BASIC', 'A', '81149189', '81149189', 'Team Head');
 
 insert into user_role (ROLE_ID, ROLE_DESC, PARENT_ROLE_ID, STATUS, CREATEBY, MODIFYBY, REMARKS)
 values ('TH__TEAM_A', 'Team A Head', 'TEAM_HEAD', 'A', '81149189', '81149189', 'Team A Head');
