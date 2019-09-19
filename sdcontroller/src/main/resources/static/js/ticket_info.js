7cf3014c62835639cdc4988d09c7fe1dc967cdfb
80883564 <Dennis.WF.Chen@pccw.com>
2019-09-19 17:52:52 +0800

    [SERVDESK-109] Find Service

--- sdcontroller/src/main/resources/static/js/ticket_info.js
+++ sdcontroller/src/main/resources/static/js/ticket_info.js
@@ -151,6 +151,69 @@ $().ready(function(){
             showErrorMsg(responseError);
         })
     });
+
+    $('#btnSearchService').on('click',function(){
+        let modal_table_body =$('.modal').find('tbody');
+        modal_table_body.empty();
+        $.post('/ticket/searchCustomer',{
+            searchKey : 'bes_custCode',
+            searchValue : customerCode
+        },function(res){
+            $.each(res,function(i,val){
+                let tr ='<tr class="text-center"></tr>';
+                modal_table_body.append(tr);
+                modal_table_body.find('tr:last').data('info',val);
+                modal_table_body.find('tr:last')
+                                .append('<td style="width:6%"><input type="radio"></td>')
+                                .append('<td>'+val.custName+'</td>')
+                                .append('<td>'+val.statusDesc+'</td>');
+            })
+            $('.modal').modal('show');
+        }).fail(function(e){
+          var responseError = e.responseText ? e.responseText : "Get failed.";
+          console.log("ERROR : ", responseError);
+          modal_table_body.append('<tr class="text-center"><td></td><td colspan=2 >no record found.</td></tr>')
+          $('.modal').modal('show');
+        })
+    })
+
+    $('#btnApplyProduct').on('click',function(){
+        let tr =$(this).parent().find('input:checked').parents('tr');
+        if(tr.length<1){
+            alert('Please select one product.');
+            return;
+        }
+        let product_info = tr.data('info');
+        $('#service').find('input[name=serviceCode]').val(product_info.serviceNo);
+        $('#service').find('input[name=serviceType]').val(product_info.serviceType);
+        if(product_info.url){
+            $('.itsm_link').data('url',product_info.url);
+            $('.itsm_link').show();
+        }
+        $('.modal').modal('hide');
+    })
+
+    $('.itsm_link').on('click',function(){
+        window.open($('.itsm_link').data('url'),'Profile','scrollbars=yes,height=600,width=800');
+    })
+
+    $.post('/ticket/searchCustomer',{
+        searchKey : 'bes_custCode',
+        searchValue : customerCode
+    },function(res){
+        if(res.length == 1){
+            $.each(res[0],function(key,value){
+                if(key =='serviceNo'){
+                    $('#service').find('input[name=serviceCode]').val(value);
+                }else{
+                    $('#service').find('input[name='+key+']').val(value);
+                }
+            })
+        }
+    }).fail(function(e){
+      var responseError = e.responseText ? e.responseText : "Get failed.";
+      console.log("ERROR : ", responseError);
+    })
 })
 
 
