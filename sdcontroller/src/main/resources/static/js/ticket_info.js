cd61e024abd6a1287034c0a837f8735ced67e6ea
Dennis Chen <Dennis.WF.Chen@pccw.com>
2019-10-17 11:16:14 +0800

    [SERVDESK-137] Ticket Status update.

--- sdcontroller/src/main/resources/static/js/ticket_info.js
+++ sdcontroller/src/main/resources/static/js/ticket_info.js
@@ -157,16 +157,10 @@ $().ready(function(){
 
     readyForTicketService();
 
-    // for test
-//    itsmUrl = 'https://10.111.7.32/itsm/info/ResourcePoolTab.action?resourceId=309033';
-
-    if(itsmUrl ==''){
-        $('.itsm_link').addClass("disabled");
-    }
-    $('.itsm_link').on('click',function(){
-        window.open(itsmUrl,'Profile','scrollbars=yes,height=600,width=800');
+    // service link button
+    $('#btnServiceLink').on('click',function(){
+        window.open($(this).data('url'),'Profile','scrollbars=yes,height=600,width=800');
     })
-
 
     // appointment
     $('#asap_checkbox').change(function(){
@@ -216,10 +210,9 @@ $().ready(function(){
             dataType: 'json',
             data: ticket,
             success:function(res){
-                $.each(res,function(key,val){
-                    $('input[name='+ key +']').val(val);
-                })
-                $('#btnTicketSubmit').attr('disabled',true);
+                if(res.success){
+                    location.reload();
+                }
             }
         }).fail(function(e){
             var responseError = e.responseText ? e.responseText : "Get failed.";
@@ -227,7 +220,7 @@ $().ready(function(){
             showErrorMsg(responseError);
         })
     })
-    // cancel ticket
+    // cancel button
     $('#btnTicketCancel').on('click',function(){
         $.post('/ticket/cancel',{
             ticketMasId:ticketMasId
