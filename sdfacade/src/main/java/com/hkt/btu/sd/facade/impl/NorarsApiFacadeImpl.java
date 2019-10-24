37bfa0c2f9ce2711474ea18d3ccd3d3daaa76673
Jason Kong <jason.yh.kong@pccw.com>
2019-10-24 16:44:31 +0800

    [SERVDESK-168] NORA - Get Related Offer Info List by BSN - rename nora return object - add button on service search page

--- sdfacade/src/main/java/com/hkt/btu/sd/facade/impl/NorarsApiFacadeImpl.java
+++ sdfacade/src/main/java/com/hkt/btu/sd/facade/impl/NorarsApiFacadeImpl.java
@@ -1,10 +1,14 @@
 package com.hkt.btu.sd.facade.impl;
 
+import com.google.gson.Gson;
+import com.hkt.btu.common.core.exception.InvalidInputException;
 import com.hkt.btu.sd.core.service.SdApiService;
 import com.hkt.btu.sd.core.service.bean.SiteInterfaceBean;
 import com.hkt.btu.sd.facade.AbstractRestfulApiFacade;
 import com.hkt.btu.sd.facade.NorarsApiFacade;
-import com.hkt.btu.sd.facade.data.NorarsBsnData;
+import com.hkt.btu.sd.facade.data.nora.NoraDnGroupData;
+import com.hkt.btu.sd.facade.data.nora.NorarsBsnData;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.annotation.Resource;
 import javax.ws.rs.client.Invocation;
@@ -14,7 +18,6 @@ import javax.ws.rs.core.MediaType;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import java.util.StringJoiner;
 
 public class NorarsApiFacadeImpl extends AbstractRestfulApiFacade implements NorarsApiFacade {
 
@@ -32,6 +35,10 @@ public class NorarsApiFacadeImpl extends AbstractRestfulApiFacade implements Nor
 
     @Override
     public String getInventory(String bsn) {
+        if(StringUtils.isEmpty(bsn)){
+            throw new InvalidInputException("Empty BSN.");
+        }
+
         String apiPath = "nora/wfm/Profile.action";
 
         Map<String, String> queryParam = new HashMap<>(2);
@@ -39,6 +46,21 @@ public class NorarsApiFacadeImpl extends AbstractRestfulApiFacade implements Nor
         queryParam.put("status", STATUS);
 
         return Optional.ofNullable(getData(apiPath, queryParam)).orElse("<h1>Test</h1>");
+    }
+
+    @Override
+    public NoraDnGroupData getRelatedOfferInfoListByBsn(String bsn) {
+        if(StringUtils.isEmpty(bsn)){
+            throw new InvalidInputException("Empty BSN.");
+        }
+
+        String apiPath = "/norars/api/v1/ec/groupids/sr/"+bsn;
+        String responseString = getData(apiPath, null);
+        if(StringUtils.isEmpty(responseString)){
+            return null;
+        } else {
+            return new Gson().fromJson(responseString, NoraDnGroupData.class);
+        }
     }
 
     @Override
