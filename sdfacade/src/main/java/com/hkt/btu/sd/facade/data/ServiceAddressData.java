676544474a8c7c98da0287064b0fcd2b049b87ff
Jason Kong <jason.yh.kong@pccw.com>
2019-10-25 10:39:48 +0800

    [SERVDESK-151] NORA - Get Service Address - add todo

---
+++ sdfacade/src/main/java/com/hkt/btu/sd/facade/data/ServiceAddressData.java
@@ -0,0 +1,33 @@
+package com.hkt.btu.sd.facade.data;
+
+public class ServiceAddressData {
+
+    private String serviceAddresss;
+    private String gridId;
+    private String exchangeBuildingId;
+
+
+    public String getServiceAddresss() {
+        return serviceAddresss;
+    }
+
+    public void setServiceAddresss(String serviceAddresss) {
+        this.serviceAddresss = serviceAddresss;
+    }
+
+    public String getGridId() {
+        return gridId;
+    }
+
+    public void setGridId(String gridId) {
+        this.gridId = gridId;
+    }
+
+    public String getExchangeBuildingId() {
+        return exchangeBuildingId;
+    }
+
+    public void setExchangeBuildingId(String exchangeBuildingId) {
+        this.exchangeBuildingId = exchangeBuildingId;
+    }
+}
