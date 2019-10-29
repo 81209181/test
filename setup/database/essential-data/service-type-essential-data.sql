3eb184dfb214f13d8c1c4f583ef683fd52925170
Jason Kong <jason.yh.kong@pccw.com>
2019-10-29 18:48:07 +0800

    [SERVDESK-175] Import symptom - update service type name

--- setup/database/essential-data/service-type-essential-data.sql
+++ setup/database/essential-data/service-type-essential-data.sql
@@ -2,11 +2,11 @@
 insert into service_type(service_type_code,service_type_name,createby,modifyby)
 values('E_CLOUD','Enterprise Cloud','system','system');
 insert into service_type(service_type_code,service_type_name,createby,modifyby)
+values('EC_365','Enterprise Cloud 365','system','system');
+insert into service_type(service_type_code,service_type_name,createby,modifyby)
 values('VOIP','voice VoIP','system','system');
 insert into service_type(service_type_code,service_type_name,createby,modifyby)
 values('BN','Broadaband','system','system');
-insert into service_type(service_type_code,service_type_name,createby,modifyby)
-values('E_365','E-Cloud 365','system','system');
 
 -- ONE
 insert into service_type_offer_mapping(service_type_code,offer_name,createby)
