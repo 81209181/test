2db8dbf358352ca2649d6acab8e0acff0e8091e8
Jason Kong <jason.yh.kong@pccw.com>
2019-10-29 16:57:33 +0800

    [SERVDESK-178] Site Config - VT - add server domain

---
+++ setup/database/essential-data/uat/uat-config-param-essential-data.sql
@@ -0,0 +1,3 @@
+-- Site Config
+insert into CONFIG_PARAM(CONFIG_GROUP, CONFIG_KEY, CONFIG_VALUE, CONFIG_VALUE_TYPE, CREATEDATE, CREATEBY, MODIFYDATE, MODIFYBY)
+values ('SITE', 'givenDomain', 'yecvm-sauat01.pccw.com', 'String', sysdate, 0, sysdate, 0);
