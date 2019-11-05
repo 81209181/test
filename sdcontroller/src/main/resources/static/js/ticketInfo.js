1c3727f4805ef7160a65cdc7ec351cda958bd9f5
Jason Tan <jason.sp.tan@pccw.com>
2019-11-05 15:06:33 +0800

    [SERVDESK-182] Roll back make appointment.

--- sdcontroller/src/main/resources/static/js/ticketInfo.js
+++ sdcontroller/src/main/resources/static/js/ticketInfo.js
@@ -321,18 +321,7 @@ function removeContact(btn){
 
 function makeAppointment(ticketMasId, ticketDetId) {
 
-    $.ajax({
-        type: 'GET',
-        url: '/ticket/makeAppointment',
-        dataType: 'text',
-        success: function (res) {
-            let inventoryWindow = window.open('', 'Inventory', 'scrollbars=yes,width=800, height=800');
-            inventoryWindow.document.write(res);
-            inventoryWindow.focus();
-        }
-    })
-
-    /*let window = AppointmentSDObj.make({
+    let window = AppointmentSDObj.make({
         data : {
             ticketMasId : ticketMasId,
             ticketDetId : ticketDetId,
@@ -342,7 +331,7 @@ function makeAppointment(ticketMasId, ticketDetId) {
             password : "Ki6=rEDs47*^5" // todo [SERVDESK-182]: need to use config param, and hide in backend
         }
     }, "https://10.252.15.158/wfm"); // todo [SERVDESK-182]: need to use config param link
-    checkWindowClose(window);*/
+    checkWindowClose(window);
 }
 
 function getInventory(bsn) {
