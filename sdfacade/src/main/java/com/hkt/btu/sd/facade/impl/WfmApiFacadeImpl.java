f62f5eeca53d9ed0ef16f36c69611681fc98cc69
Jason Kong <jason.yh.kong@pccw.com>
2019-11-18 17:09:33 +0800

    handle wfm api response fail

--- sdfacade/src/main/java/com/hkt/btu/sd/facade/impl/WfmApiFacadeImpl.java
+++ sdfacade/src/main/java/com/hkt/btu/sd/facade/impl/WfmApiFacadeImpl.java
@@ -56,7 +56,7 @@ public class WfmApiFacadeImpl extends AbstractRestfulApiFacade implements WfmApi
         Response res = postEntity("/api/v1/sd/FaultCreate", Entity.entity(JsonUtils.getMapperFormatLocalDateTime2String().writeValueAsString(ticketData), MediaType.APPLICATION_JSON));
         WfmResponse wfmRes = res.readEntity(WfmResponse.class);
         return Optional.ofNullable(wfmRes.getData()).map(WfmSuccess::getJobId).orElseThrow(() ->
-                new RuntimeException(String.format("WFM Error: %s . Please contact WFM Admin.", wfmRes.getErrorMsg())));
+                new RuntimeException(String.format("WFM Error: %s. Please contact WFM Admin.", wfmRes.getErrorMsg())));
     }
 
     @Override
@@ -101,7 +101,7 @@ public class WfmApiFacadeImpl extends AbstractRestfulApiFacade implements WfmApi
         }
 
         if (responseData == null) {
-            String errorMsg = "WFM Error: No response";
+            String errorMsg = "WFM Error: No response for pending order.";
             LOG.error(errorMsg);
 
             responseData = new WfmPendingOrderData();
