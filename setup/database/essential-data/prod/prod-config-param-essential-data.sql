9b890eca4db51f23b3dfe8a1da0223819e3cbee7
Jason Kong <jason.yh.kong@pccw.com>
2019-11-27 16:30:25 +0800

    Add old api key ref from old db

--- setup/database/essential-data/prod/prod-config-param-essential-data.sql
+++ setup/database/essential-data/prod/prod-config-param-essential-data.sql
@@ -1,14 +1,15 @@
 -- Site Config
 insert into CONFIG_PARAM(CONFIG_GROUP, CONFIG_KEY, CONFIG_VALUE, CONFIG_VALUE_TYPE, CREATEDATE, CREATEBY, MODIFYDATE, MODIFYBY)
-values ('SITE', 'givenDomain', 'yecvm-sauat01.pccw.com', 'String', sysdate, 0, sysdate, 0);
+values ('SITE', 'givenDomain', 'sd.pccw.com', 'String', sysdate, 0, sysdate, 0);
 
 -- oldTable.API_AUTHORIZATION (for ref)
 -- basic,bes,jsa9u3vhs1
 -- basic,hktcloud,hktcloud20180515
 -- basic,wfm,wfmwfm123
 
+
 -- oldTable.SITE_INTERFACE (for ref)
--- ITSM_RESTFUL,https://10.111.7.30:8443,ssa,sa2018,,,,,
--- WFM,https://10.111.7.31:8443/wfm,sd,Ki6/rEDs47*^5,,,,,
+-- ITSM_RESTFUL,https://itsm.pccw.com/cloudrs,sa,sa2018,,,,,
+-- WFM,https://wfm.pccw.com/wfm,sd,Ki6/rEDs47*^5,,,,,
 -- TEST_LINE,http://10.252.16.151:8080,,,,,,,
--- ITSM,https://10.111.7.30:8443/itsm,,,,,,,
+-- ITSM,https://itsm.pccw.com/itsm,,,,,,,
--- setup/database/essential-data/sit/sit-config-param-essential-data.sql
+++ setup/database/essential-data/sit/sit-config-param-essential-data.sql
@@ -2,3 +2,16 @@
 insert into CONFIG_PARAM(CONFIG_GROUP, CONFIG_KEY, CONFIG_VALUE, CONFIG_VALUE_TYPE, CREATEDATE, CREATEBY, MODIFYDATE, MODIFYBY)
 values ('SITE', 'givenDomain', 'yecvm-sasit01.pccw.com', 'String', sysdate, 0, sysdate, 0);
 
+
+-- oldTable.API_AUTHORIZATION (for ref)
+-- basic,bes,jsa9u3vhs1
+-- basic,hktcloud,hktcloud20180515
+-- basic,wfm,wfmwfm123
+
+-- oldTable.SITE_INTERFACE (for ref)
+-- ITSM_RESTFUL,https://10.111.7.32,ssa,sa2018,,,,,
+-- WFM,https://10.111.6.34/wfm,sd,Ki6/rEDs47*^5,,,,,
+-- TEST_LINE,http://10.252.16.151:8080,,,,,,,
+-- ITSM,https://10.111.7.32/itsm,,,,,,,
+-- NORARS,https://10.252.15.139,sd,g$Hw0#MG-3,2019-08-07 12:02:36,01634476,,,
+-- BES,https://10.50.36.110:2443,999007,Bearer 5b4bf68e5dae6376ac9d569b31b09800,2019-05-10 11:01:40,01634476,16fdcfc5512645ebab925357499928ff,101,613
