aa5efd701c4cceb001b527748b01c84afc856538
Jason Kong <jason.yh.kong@pccw.com>
2019-12-03 17:36:55 +0800

    [SERVDESK-257] Check Ticket Remark Input Length - updated to check utf8 byte size

--- sdfacade/src/main/java/com/hkt/btu/sd/facade/impl/SdTicketFacadeImpl.java
+++ sdfacade/src/main/java/com/hkt/btu/sd/facade/impl/SdTicketFacadeImpl.java
@@ -32,11 +32,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeParseException;
 import java.util.*;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class SdTicketFacadeImpl implements SdTicketFacade {
@@ -277,20 +277,11 @@ public class SdTicketFacadeImpl implements SdTicketFacade {
     @Override
     public void createTicketRemarks(Integer ticketMasId, String remarks) {
         if (ticketMasId == null) {
-            throw new RuntimeException("Ticket Mas ID is empty.");
-        }
-        if (StringUtils.isEmpty(remarks)) {
-            throw new RuntimeException("Remarks is empty.");
-        }
-        Pattern p = Pattern.compile("[\u4e00-\u9fa5]");
-        if (p.matcher(remarks).find()) {
-            if (remarks.length() > 166) {
-                throw new RuntimeException("Input remark is too long. (max: 500 characters / 166 chinese words)");
-            }
-        } else {
-            if (remarks.length() > 500) {
-                throw new RuntimeException("Input remark is too long. (max: 500 characters / 166 chinese words)");
-            }
+            throw new InvalidInputException("Ticket Mas ID is empty.");
+        } else if (StringUtils.isEmpty(remarks)) {
+            throw new InvalidInputException("Remarks is empty.");
+        } else if (remarks.getBytes(StandardCharsets.UTF_8).length > 500){
+            throw new InvalidInputException("Input remark is too long. (max: 500 characters / 166 chinese words)");
         }
 
         try {
