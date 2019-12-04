2ec1c16044810a9486bde05cd974f8d73f28ab38
Jason Kong <jason.yh.kong@pccw.com>
2019-12-04 15:29:58 +0800

    [SERVDESK-265] Prepare API key to other system (incoming) - update role-pathCtrl setting

--- setup/database/essential-data/path-ctrl-essential-data.sql
+++ setup/database/essential-data/path-ctrl-essential-data.sql
@@ -1,7 +1,7 @@
 -- AntPath mapping control
 insert into PATH_CTRL(PATH, STATUS, DESCRIPTION,CREATEBY, MODIFYBY) values('/user/**', 'A', 'Basic Path', '81149189', '81149189');
 insert into PATH_CTRL(PATH, STATUS, DESCRIPTION,CREATEBY, MODIFYBY) values('/**', 'A', 'System Admin', '81149189', '81149189');
-insert into PATH_CTRL(PATH, STATUS, DESCRIPTION,CREATEBY, MODIFYBY) values('/fault/**', 'A', 'CS Agent', '81149189', '81149189');
+insert into PATH_CTRL(PATH, STATUS, DESCRIPTION,CREATEBY, MODIFYBY) values('/ticket/**', 'A', 'CS Agent', '81149189', '81149189');
 insert into PATH_CTRL(PATH, STATUS, DESCRIPTION,CREATEBY, MODIFYBY) values('/admin/manage-user/**', 'A', 'Team Head', '81149189', '81149189');
 insert into PATH_CTRL(PATH, STATUS, DESCRIPTION,CREATEBY, MODIFYBY) values('/wfm-api/**', 'A', 'WFM API', '01634476', '01634476');
 insert into PATH_CTRL(PATH, STATUS, DESCRIPTION,CREATEBY, MODIFYBY) values('/bes-api/**', 'A', 'BES API', '01634476', '01634476');
