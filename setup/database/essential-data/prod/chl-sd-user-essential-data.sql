ed0e385765f292cf0de9096505c74249ba241d7e
Rico Liu <Rico.WX.Liu@pccw.com>
2019-12-05 12:14:40 +0800

    [SERVDESK-267] Prepare CHL User

---
+++ setup/database/essential-data/prod/chl-sd-user-essential-data.sql
@@ -0,0 +1,39 @@
+insert into USER_PROFILE (USER_ID, NAME, STATUS, MOBILE, EMAIL, LDAP_DOMAIN, CREATEBY, MODIFYBY, DOMAIN_EMAIL, PRIMARY_ROLE_ID)
+values ('00489716', 'Lew, Ricky PW', 'A', '28835689', 'ricky.pw.lew@pccw.com', '@corphq.hk.pccw.com','81209181','81209181','ricky.pw.lew@pccw.com', 'CHL');
+insert into USER_USER_ROLE (USER_ID, ROLE_ID) values ('00489716','CHL');
+
+insert into USER_PROFILE (USER_ID, NAME, STATUS, MOBILE, EMAIL, LDAP_DOMAIN, CREATEBY, MODIFYBY, DOMAIN_EMAIL, PRIMARY_ROLE_ID)
+values ('00168245', 'Chan, Sau-Wah', 'A', '28839357', 'sau-wah.chan@pccw.com', '@corphq.hk.pccw.com','81209181','81209181','sau-wah.chan@pccw.com', 'CHL');
+insert into USER_USER_ROLE (USER_ID, ROLE_ID) values ('00168245','CHL');
+
+insert into USER_PROFILE (USER_ID, NAME, STATUS, MOBILE, EMAIL, LDAP_DOMAIN, CREATEBY, MODIFYBY, DOMAIN_EMAIL, PRIMARY_ROLE_ID)
+values ('00940585', 'Leung, Laura PK', 'A', '28839964', 'laura.pk.leung@pccw.com', '@corphq.hk.pccw.com','81209181','81209181','laura.pk.leung@pccw.com', 'TH__O_CHL');
+insert into USER_USER_ROLE (USER_ID, ROLE_ID) values ('00940585','TH__O_CHL');
+
+insert into USER_PROFILE (USER_ID, NAME, STATUS, MOBILE, EMAIL, LDAP_DOMAIN, CREATEBY, MODIFYBY, DOMAIN_EMAIL, PRIMARY_ROLE_ID)
+values ('80394570', 'Chiu, Candy KK', 'A', '34734128', 'candy.kk.chiu@pccw.com', '@corphq.hk.pccw.com','81209181','81209181','candy.kk.chiu@pccw.com', 'CHL');
+insert into USER_USER_ROLE (USER_ID, ROLE_ID) values ('80394570','CHL');
+
+insert into USER_PROFILE (USER_ID, NAME, STATUS, MOBILE, EMAIL, LDAP_DOMAIN, CREATEBY, MODIFYBY, DOMAIN_EMAIL, PRIMARY_ROLE_ID)
+values ('80904006', 'Lau, Vincent WH', 'A', '', 'Vincet.wh.lau@pccw.com', '@corphq.hk.pccw.com','81209181','81209181','Vincet.wh.lau@pccw.com', 'CHL');
+insert into USER_USER_ROLE (USER_ID, ROLE_ID) values ('80904006','CHL');
+
+insert into USER_PROFILE (USER_ID, NAME, STATUS, MOBILE, EMAIL, LDAP_DOMAIN, CREATEBY, MODIFYBY, DOMAIN_EMAIL, PRIMARY_ROLE_ID)
+values ('80029440', 'Leung, Alice LS', 'A', '28839357', 'alice.ls.leung@pccw.com', '@corphq.hk.pccw.com','81209181','81209181','alice.ls.leung@pccw.com', 'CHL');
+insert into USER_USER_ROLE (USER_ID, ROLE_ID) values ('80029440','CHL');
+
+insert into USER_PROFILE (USER_ID, NAME, STATUS, MOBILE, EMAIL, LDAP_DOMAIN, CREATEBY, MODIFYBY, DOMAIN_EMAIL, PRIMARY_ROLE_ID)
+values ('80156052', 'Tam, Kammy CM', 'A', '28839357', 'Kammy.cm.tam@pccw.com', '@corphq.hk.pccw.com','81209181','81209181','Kammy.cm.tam@pccw.com', 'CHL');
+insert into USER_USER_ROLE (USER_ID, ROLE_ID) values ('80156052','CHL');
+
+insert into USER_PROFILE (USER_ID, NAME, STATUS, MOBILE, EMAIL, LDAP_DOMAIN, CREATEBY, MODIFYBY, DOMAIN_EMAIL, PRIMARY_ROLE_ID)
+values ('00699579', 'Cheng, Stephen CK', 'A', '28833345', 'stephen.ck.cheng@pccw.com', '@corphq.hk.pccw.com','81209181','81209181','stephen.ck.cheng@pccw.com', 'CHL');
+insert into USER_USER_ROLE (USER_ID, ROLE_ID) values ('00699579','CHL');
+
+insert into USER_PROFILE (USER_ID, NAME, STATUS, MOBILE, EMAIL, LDAP_DOMAIN, CREATEBY, MODIFYBY, DOMAIN_EMAIL, PRIMARY_ROLE_ID)
+values ('00648030', 'Chan, Wai-Keung WK2', 'A', '28832515', 'wai-keung.wk2.chan@pccw.com', '@corphq.hk.pccw.com','81209181','81209181','wai-keung.wk2.chan@pccw.com', 'CHL');
+insert into USER_USER_ROLE (USER_ID, ROLE_ID) values ('00648030','CHL');
+
+insert into USER_PROFILE (USER_ID, NAME, STATUS, MOBILE, EMAIL, LDAP_DOMAIN, CREATEBY, MODIFYBY, DOMAIN_EMAIL, PRIMARY_ROLE_ID)
+values ('01110253', 'Liu, Tsz-On', 'A', '27539984', 'tsz-on.liu@pccw.com', '@corphq.hk.pccw.com','81209181','81209181','tsz-on.liu@pccw.com', 'CHL');
+insert into USER_USER_ROLE (USER_ID, ROLE_ID) values ('01110253','CHL');
