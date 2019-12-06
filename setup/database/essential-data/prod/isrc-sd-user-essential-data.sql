f60cf0c997999327650c8a031a9b2feb7905ac51
Rico Liu <Rico.WX.Liu@pccw.com>
2019-12-06 15:04:36 +0800

    [SERVDESK-276] Update Prod User

--- setup/database/essential-data/prod/isrc-sd-user-essential-data.sql
+++ setup/database/essential-data/prod/isrc-sd-user-essential-data.sql
@@ -1,1 +1,34 @@
--- E_ISRC_V
+insert into USER_PROFILE (USER_ID, NAME, STATUS, MOBILE, EMAIL, LDAP_DOMAIN, CREATEBY, MODIFYBY, DOMAIN_EMAIL, PRIMARY_ROLE_ID)
+values ('00291195', 'Tang Siu Lun', 'A', '28889562', '', '@corphq.hk.pccw.com','81209181','81209181','', 'E_ISRC_V');
+insert into USER_USER_ROLE (USER_ID, ROLE_ID) values ('00291195','TH__E_ISRC_V');
+insert into USER_USER_ROLE (USER_ID, ROLE_ID) values ('00291195','E_ISRC_V');
+
+insert into USER_PROFILE (USER_ID, NAME, STATUS, MOBILE, EMAIL, LDAP_DOMAIN, CREATEBY, MODIFYBY, DOMAIN_EMAIL, PRIMARY_ROLE_ID)
+values ('01620590', 'Kam Ho Ming, Victor', 'A', '28889562', '', '@corphq.hk.pccw.com','81209181','81209181','', 'E_ISRC_V');
+insert into USER_USER_ROLE (USER_ID, ROLE_ID) values ('01620590','TH__E_ISRC_V');
+insert into USER_USER_ROLE (USER_ID, ROLE_ID) values ('01620590','E_ISRC_V');
+
+insert into USER_PROFILE (USER_ID, NAME, STATUS, MOBILE, EMAIL, LDAP_DOMAIN, CREATEBY, MODIFYBY, DOMAIN_EMAIL, PRIMARY_ROLE_ID)
+values ('01575828', 'Tam Ching Lam', 'A', '28889702', '', '@corphq.hk.pccw.com','81209181','81209181','', 'E_ISRC_V');
+insert into USER_USER_ROLE (USER_ID, ROLE_ID) values ('01575828','E_ISRC_V');
+
+insert into USER_PROFILE (USER_ID, NAME, STATUS, MOBILE, EMAIL, LDAP_DOMAIN, CREATEBY, MODIFYBY, DOMAIN_EMAIL, PRIMARY_ROLE_ID)
+values ('01630417', 'Wong Hoi Lee', 'A', '28889702', 'Kylie.HL.Wong@pccw.com', '@corphq.hk.pccw.com','81209181','81209181','Kylie.HL.Wong@pccw.com', 'E_ISRC_V');
+insert into USER_USER_ROLE (USER_ID, ROLE_ID) values ('01630417','E_ISRC_V');
+
+insert into USER_PROFILE (USER_ID, NAME, STATUS, MOBILE, EMAIL, LDAP_DOMAIN, CREATEBY, MODIFYBY, DOMAIN_EMAIL, PRIMARY_ROLE_ID)
+values ('01697242', 'Lee Yeuk Lam', 'A', '28889702', 'Joyce.YL.Lee@pccw.com', '@corphq.hk.pccw.com','81209181','81209181','Joyce.YL.Lee@pccw.com', 'E_ISRC_V');
+insert into USER_USER_ROLE (USER_ID, ROLE_ID) values ('01697242','E_ISRC_V');
+
+insert into USER_PROFILE (USER_ID, NAME, STATUS, MOBILE, EMAIL, LDAP_DOMAIN, CREATEBY, MODIFYBY, DOMAIN_EMAIL, PRIMARY_ROLE_ID)
+values ('00643411', 'Li Wan Wah', 'A', '28882889', 'Wan-Wah.Li@pccw.com', '@corphq.hk.pccw.com','81209181','81209181','Wan-Wah.Li@pccw.com', 'E_ISRC_V');
+insert into USER_USER_ROLE (USER_ID, ROLE_ID) values ('00643411','E_ISRC_V');
+
+insert into USER_PROFILE (USER_ID, NAME, STATUS, MOBILE, EMAIL, LDAP_DOMAIN, CREATEBY, MODIFYBY, DOMAIN_EMAIL, PRIMARY_ROLE_ID)
+values ('01435759', 'Pun Yiu Ming', 'A', '28882889', 'Yiu-Ming.Pun@pccw.com', '@corphq.hk.pccw.com','81209181','81209181','Yiu-Ming.Pun@pccw.com', 'E_ISRC_V');
+insert into USER_USER_ROLE (USER_ID, ROLE_ID) values ('01435759','E_ISRC_V');
+
+insert into USER_PROFILE (USER_ID, NAME, STATUS, MOBILE, EMAIL, LDAP_DOMAIN, CREATEBY, MODIFYBY, DOMAIN_EMAIL, PRIMARY_ROLE_ID)
+values ('01599174', 'Leung Cheuk Fai', 'A', '28882899', 'Kevin.Leung@pccw.com', '@corphq.hk.pccw.com','81209181','81209181','Kevin.Leung@pccw.com', 'E_ISRC_V');
+insert into USER_USER_ROLE (USER_ID, ROLE_ID) values ('01599174','E_ISRC_V');
+
