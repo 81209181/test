409ec5ef0327398384a74980b2b6903bc138ec56
Jason Kong <jason.yh.kong@pccw.com>
2019-12-11 12:58:43 +0800

    [SERVDESK-282] CheckTimeSyncJob - optimize import

--- sdcore/src/main/java/com/hkt/btu/sd/core/job/SdCheckTimeSyncJob.java
+++ sdcore/src/main/java/com/hkt/btu/sd/core/job/SdCheckTimeSyncJob.java
@@ -3,7 +3,6 @@ package com.hkt.btu.sd.core.job;
 import com.hkt.btu.sd.core.service.SdHealthCheckService;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 import javax.annotation.Resource;
