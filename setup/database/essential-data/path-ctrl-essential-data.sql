fdafcca387f79f2ffcc892293a2f4890000d6ee9
Jason Kong <jason.yh.kong@pccw.com>
2019-12-12 18:18:46 +0800

    [SERVDESK-280] Team Head List - add path control essential data

--- setup/database/essential-data/path-ctrl-essential-data.sql
+++ setup/database/essential-data/path-ctrl-essential-data.sql
@@ -2,8 +2,9 @@
 insert into PATH_CTRL(PATH, STATUS, DESCRIPTION,CREATEBY, MODIFYBY) values('/user/**', 'A', 'Basic Path', '81149189', '81149189');
 insert into PATH_CTRL(PATH, STATUS, DESCRIPTION,CREATEBY, MODIFYBY) values('/**', 'A', 'System Admin', '81149189', '81149189');
 insert into PATH_CTRL(PATH, STATUS, DESCRIPTION,CREATEBY, MODIFYBY) values('/ticket/**', 'A', 'Ticket', '81149189', '81149189');
-insert into PATH_CTRL(PATH, STATUS, DESCRIPTION,CREATEBY, MODIFYBY) values('/symptom/**', 'A', 'Ticket', '81149189', '81149189');
-insert into PATH_CTRL(PATH, STATUS, DESCRIPTION,CREATEBY, MODIFYBY) values('/admin/manage-user/**', 'A', 'Team Head', '81149189', '81149189');
+insert into PATH_CTRL(PATH, STATUS, DESCRIPTION,CREATEBY, MODIFYBY) values('/symptom/**', 'A', 'Symptom', '81149189', '81149189');
+insert into PATH_CTRL(PATH, STATUS, DESCRIPTION,CREATEBY, MODIFYBY) values('/admin/manage-user/**', 'A', 'Manage User', '81149189', '81149189');
+insert into PATH_CTRL(PATH, STATUS, DESCRIPTION,CREATEBY, MODIFYBY) values('/admin/manage-role/**', 'A', 'Manage User Role', '81149189', '81149189');
 insert into PATH_CTRL(PATH, STATUS, DESCRIPTION,CREATEBY, MODIFYBY) values('/wfm-api/**', 'A', 'WFM API', '01634476', '01634476');
 insert into PATH_CTRL(PATH, STATUS, DESCRIPTION,CREATEBY, MODIFYBY) values('/bes-api/**', 'A', 'BES API', '01634476', '01634476');
 commit;
