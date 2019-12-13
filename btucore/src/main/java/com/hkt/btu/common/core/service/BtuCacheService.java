15d98869fa3972e25dda59dc7381ebaeb1e9257c
Dennis Chen <Dennis.WF.Chen@pccw.com>
2019-12-13 11:00:34 +0800

    test.

--- btucore/src/main/java/com/hkt/btu/common/core/service/BtuCacheService.java
+++ btucore/src/main/java/com/hkt/btu/common/core/service/BtuCacheService.java
@@ -1,11 +1,7 @@
 package com.hkt.btu.common.core.service;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 public interface BtuCacheService {
 
-    <T> T get(Class<T> objectType, Object loadingClass, Method loadingMethod) throws InvocationTargetException, IllegalAccessException;
-
+    String getCache(String cacheName);
 
 }
