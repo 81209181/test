7741b1495699c634cedc79d4b5c9edd632dd9bd3
Dennis Chen <Dennis.WF.Chen@pccw.com>
2019-12-25 14:19:45 +0800

    [SERVDESK-307] Job - Check Cert Job updated.

---
+++ sdcore/src/main/java/com/hkt/btu/sd/core/job/SdCheckCertJob.java
@@ -0,0 +1,42 @@
+package com.hkt.btu.sd.core.job;
+
+import com.hkt.btu.common.core.service.BtuSiteConfigService;
+import com.hkt.btu.common.core.service.bean.BtuSiteConfigBean;
+import com.hkt.btu.sd.core.service.SdCertService;
+import com.hkt.btu.sd.core.service.SdEmailService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import javax.annotation.Resource;
+import javax.mail.MessagingException;
+
+@DisallowConcurrentExecution
+public class SdCheckCertJob extends QuartzJobBean {
+    private static final Logger LOG = LogManager.getLogger(SdCheckCertJob.class);
+
+    @Resource(name = "siteConfigService")
+    BtuSiteConfigService siteConfigService;
+    @Resource(name = "certService")
+    SdCertService certService;
+    @Resource(name = "emailService")
+    SdEmailService emailService;
+
+    @Override
+    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        String appHttpUrl = siteConfigService.getSiteConfigBean().getAppHttpsUrl();
+        try {
+            certService.checkCert(appHttpUrl);
+        } catch (RuntimeException e) {
+            try {
+                emailService.send(BtuSiteConfigBean.DEFAULT_MAIL_USERNAME,"SdCheckCertJob report",e.getMessage());
+            } catch (MessagingException ex) {
+                LOG.warn(ex);
+            }
+        }
+
+    }
+}
---
+++ sdcore/src/main/java/com/hkt/btu/sd/core/service/SdCertService.java
@@ -0,0 +1,6 @@
+package com.hkt.btu.sd.core.service;
+
+public interface SdCertService {
+
+    void checkCert(String url);
+}
