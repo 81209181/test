0e7dcc4feed817fee9967f0f3291b02df409dd89
Rico Liu <Rico.WX.Liu@pccw.com>
2020-03-16 17:47:26 +0800

    [SERVDESK-351] Complete BtuParamService

--- btucore/src/main/java/com/hkt/btu/common/core/service/impl/BtuParamServiceImpl.java
+++ btucore/src/main/java/com/hkt/btu/common/core/service/impl/BtuParamServiceImpl.java
@@ -2,7 +2,12 @@ package com.hkt.btu.common.core.service.impl;
 
 import com.hkt.btu.common.core.service.BtuParamService;
 import com.hkt.btu.common.core.service.bean.BtuParamBean;
+import com.hkt.btu.common.core.service.constant.BtuConfigParamTypeEnum;
+import com.hkt.btu.common.core.util.JsonUtils;
+import org.apache.commons.collections4.CollectionUtils;
 
+import java.time.LocalDateTime;
+import java.util.LinkedList;
 import java.util.List;
 
 public class BtuParamServiceImpl implements BtuParamService {
@@ -10,18 +15,52 @@ public class BtuParamServiceImpl implements BtuParamService {
 
     @Override
     public String serialize(Object[] objArray) {
-        List<BtuParamBean> paramList;
-        // todo [SERVDESK-351]:
-        // eg. (String, Integer)
-        // only support object type in BtuConfigParamTypeEnum
-        // 1. convert to List<BtuParamBean>
-        // 2. List<BtuParamBean> to json
-        return null;
+        List<BtuParamBean> paramList = new LinkedList<>();
+        String result = null;
+
+        if(objArray != null && objArray.length > 0){
+            for (int i = 0; i < objArray.length; i++) {
+                BtuParamBean bean = new BtuParamBean();
+                if(objArray[i] instanceof String){
+                    bean.setParamType(BtuConfigParamTypeEnum.STRING);
+                    bean.setValue(objArray[i].toString());
+                    paramList.add(bean);
+                }else if(objArray[i] instanceof Integer){
+                    bean.setParamType(BtuConfigParamTypeEnum.INTEGER);
+                    bean.setValue(objArray[i].toString());
+                    paramList.add(bean);
+                }else if(objArray[i] instanceof Double){
+                    bean.setParamType(BtuConfigParamTypeEnum.DOUBLE);
+                    bean.setValue(objArray[i].toString());
+                    paramList.add(bean);
+                }else if(objArray[i] instanceof Boolean){
+                    bean.setParamType(BtuConfigParamTypeEnum.BOOLEAN);
+                    bean.setValue(objArray[i].toString());
+                    paramList.add(bean);
+                }else if(objArray[i] instanceof LocalDateTime){
+                    bean.setParamType(BtuConfigParamTypeEnum.LOCAL_DATE_TIME);
+                    bean.setValue(objArray[i].toString());
+                    paramList.add(bean);
+                }
+            }
+        }
+
+        if(CollectionUtils.isNotEmpty(paramList)){
+            result = JsonUtils.obj2String(paramList);
+        }
+
+        return result;
     }
 
     @Override
     public Object[] deserialize(String paramListJson) {
-//        todo [SERVDESK-351]: reserve serialize
-        return new Object[0];
+        List<BtuParamBean> paramList = JsonUtils.string2Obj(paramListJson,List.class,BtuParamBean.class);
+        Object[] objArray = null;
+
+        if(CollectionUtils.isNotEmpty(paramList)){
+            objArray = paramList.toArray();
+        }
+
+        return objArray;
     }
 }
---
+++ btucore/src/main/java/com/hkt/btu/common/core/util/JsonUtils.java
@@ -0,0 +1,129 @@
+package com.hkt.btu.common.core.util;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class JsonUtils {
+
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    public static ObjectMapper getMapperFormatLocalDateTime2String() {
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern("HH:mm:ss")));
+        objectMapper.registerModule(javaTimeModule);
+        return objectMapper;
+    }
+
+    /**
+     * Object convert to json string
+     * @param obj
+     * @param <T>
+     * @return
+     */
+    public static <T> String obj2String(T obj){
+        if (obj == null){
+            return null;
+        }
+        try {
+            return obj instanceof String ? (String) obj : objectMapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            System.out.println("Parse object to String error");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Object convert to json string and formatting
+     * @param obj
+     * @param <T>
+     * @return
+     */
+    public static <T> String obj2StringPretty(T obj){
+        if (obj == null){
+            return null;
+        }
+        try {
+            return obj instanceof String ? (String) obj : objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj);
+        } catch (Exception e) {
+            System.out.println("Parse object to String error");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * String convert to object
+     * @param str json string
+     * @param clazz Conversion object class
+     * @param <T>
+     * @return
+     */
+    public static <T> T string2Obj(String str,Class<T> clazz){
+        if (StringUtils.isEmpty(str) || clazz == null){
+            return null;
+        }
+        try {
+            return clazz.equals(String.class)? (T) str :objectMapper.readValue(str,clazz);
+        } catch (IOException e) {
+            System.out.println("Parse String to Object error");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * String convert to object
+     * @param str json string
+     * @param typeReference Conversion object reference type
+     * @param <T>
+     * @return
+     */
+    public static <T> T string2Obj(String str, TypeReference<T> typeReference){
+        if (StringUtils.isEmpty(str) || typeReference == null){
+            return null;
+        }
+        try {
+            return (T)(typeReference.getType().equals(String.class)? str :objectMapper.readValue(str,typeReference));
+        } catch (IOException e) {
+            System.out.println("Parse String to Object error");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * String convert to object (Used to convert to a collection object)
+     * @param str json string
+     * @param collectionClass Conversion collection class
+     * @param elementClasses object class in conversion collection
+     * @param <T>
+     * @return
+     */
+    public static <T> T string2Obj(String str,Class<?> collectionClass,Class<?>... elementClasses){
+        JavaType javaType = objectMapper.getTypeFactory().constructParametricType(collectionClass,elementClasses);
+        try {
+            return objectMapper.readValue(str,javaType);
+        } catch (IOException e) {
+            System.out.println("Parse String to Object error");
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
