b7b35b6de4fdf72aa548f87e5e015b3c3c5a08fa
Jason Kong <jason.yh.kong@pccw.com>
2020-03-17 10:53:49 +0800

    [SERVDESK-351] Complete BtuParamService - add gson ref

--- btucore/src/main/java/com/hkt/btu/common/core/service/impl/BtuParamServiceImpl.java
+++ btucore/src/main/java/com/hkt/btu/common/core/service/impl/BtuParamServiceImpl.java
@@ -1,11 +1,15 @@
 package com.hkt.btu.common.core.service.impl;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.hkt.btu.common.core.exception.InvalidInputException;
 import com.hkt.btu.common.core.service.BtuParamService;
 import com.hkt.btu.common.core.service.bean.BtuParamBean;
 import com.hkt.btu.common.core.service.constant.BtuConfigParamTypeEnum;
 import com.hkt.btu.common.core.util.JsonUtils;
 import org.apache.commons.collections4.CollectionUtils;
 
+import java.lang.reflect.Type;
 import java.time.LocalDateTime;
 import java.util.LinkedList;
 import java.util.List;
@@ -19,34 +23,37 @@ public class BtuParamServiceImpl implements BtuParamService {
         String result = null;
 
         if(objArray != null && objArray.length > 0){
-            for (int i = 0; i < objArray.length; i++) {
+            for (Object o : objArray) {
                 BtuParamBean bean = new BtuParamBean();
-                if(objArray[i] instanceof String){
+                if (o instanceof String) {
                     bean.setParamType(BtuConfigParamTypeEnum.STRING);
-                    bean.setValue(objArray[i].toString());
+                    bean.setValue(o.toString());
                     paramList.add(bean);
-                }else if(objArray[i] instanceof Integer){
+                } else if (o instanceof Integer) {
                     bean.setParamType(BtuConfigParamTypeEnum.INTEGER);
-                    bean.setValue(objArray[i].toString());
+                    bean.setValue(o.toString());
                     paramList.add(bean);
-                }else if(objArray[i] instanceof Double){
+                } else if (o instanceof Double) {
                     bean.setParamType(BtuConfigParamTypeEnum.DOUBLE);
-                    bean.setValue(objArray[i].toString());
+                    bean.setValue(o.toString());
                     paramList.add(bean);
-                }else if(objArray[i] instanceof Boolean){
+                } else if (o instanceof Boolean) {
                     bean.setParamType(BtuConfigParamTypeEnum.BOOLEAN);
-                    bean.setValue(objArray[i].toString());
+                    bean.setValue(o.toString());
                     paramList.add(bean);
-                }else if(objArray[i] instanceof LocalDateTime){
+                } else if (o instanceof LocalDateTime) {
                     bean.setParamType(BtuConfigParamTypeEnum.LOCAL_DATE_TIME);
-                    bean.setValue(objArray[i].toString());
+                    bean.setValue(o.toString());
                     paramList.add(bean);
+                } else {
+                    throw new InvalidInputException("Unsupported type for re-try.");
                 }
             }
         }
 
         if(CollectionUtils.isNotEmpty(paramList)){
-            result = JsonUtils.obj2String(paramList);
+//            result = JsonUtils.obj2String(paramList);
+            result = new Gson().toJson(paramList);
         }
 
         return result;
@@ -54,6 +61,10 @@ public class BtuParamServiceImpl implements BtuParamService {
 
     @Override
     public Object[] deserialize(String paramListJson) {
+        // todo: reference below, use gson instead of JsonUtils
+        // com/hkt/btu/sd/facade/impl/WfmApiFacadeImpl.java.getJobRemarkByTicketId
+        // Type type = new TypeToken<List<BtuParamBean>>() {}.getType();
+
         List<BtuParamBean> paramList = JsonUtils.string2Obj(paramListJson,List.class,BtuParamBean.class);
         Object[] objArray = null;
 
